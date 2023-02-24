2eb439c3cc77ceb22ce56456a40597cae21ab846
kjartano47 <kjartano97@gmail.com>
2023-02-24 16:30:34 +0000

    fixed update functionality in httpPost

--- HotelBookingAPI/HotelBookingAPI/Controllers/HotelBookingController.cs
+++ HotelBookingAPI/HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -20,19 +20,21 @@ namespace HotelBookingAPI.Controllers
 		[HttpPost]
 		public JsonResult CreateEdit(HotelBooking booking)
 		{
-			if(booking.Id == 0)
+			if (booking.Id == 0)
 			{
 				_context.Bookings.Add(booking);
 			}
 			else
 			{
 				var bookingInDb = _context.Bookings.Find(booking.Id);
-				if(bookingInDb == null)
-				{
+				if (bookingInDb == null)
 					return new JsonResult(NotFound());
-				}
+
+				_context.Entry(bookingInDb).State = Microsoft.EntityFrameworkCore.EntityState.Detached;
 				bookingInDb = booking;
+				_context.Update(bookingInDb);
 			}
+
 			_context.SaveChanges();
 			return new JsonResult(Ok(booking));
 		}
@@ -48,6 +50,21 @@ namespace HotelBookingAPI.Controllers
 				return new JsonResult(NotFound());
 			}
 			return new JsonResult(Ok(result));
+		}
+
+		[HttpDelete]
+		public JsonResult Delete(int id)
+		{
+			var result = _context.Bookings.Find(id);
+			if(result == null)
+			{
+				return new JsonResult(NotFound());
+			}
+			_context.Bookings.Remove(result);
+			_context.SaveChanges();
+			return new JsonResult(NoContent());
+
+
 		}
 	}
 }
