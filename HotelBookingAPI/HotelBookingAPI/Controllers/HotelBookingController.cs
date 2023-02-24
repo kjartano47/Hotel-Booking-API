c92f0481ab09b1cb2b33ff46ea2fcbc5ff3de4ac
kjartano47 <kjartano97@gmail.com>
2023-02-24 15:29:18 +0000

    Added Controller

---
+++ HotelBookingAPI/HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+
+namespace HotelBookingAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HotelBookingController : ControllerBase
+	{
+	}
+}
